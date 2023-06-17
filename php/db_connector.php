cc959a01c2afa76b6e6589abafc3f4baa9ee865d
szvbvtk <52550015+szvbvtk@users.noreply.github.com>
2023-06-17 10:09:42 +0200

    Update db_connector.php

--- php/db_connector.php
+++ php/db_connector.php
@@ -1,9 +1,9 @@
 <?php 
 
 $serverName = 'localhost';
-$dbUsername = 'mszfrelbst_admin';
-$dbPassword = '7-mG_#X]Jp-vcHN5';
-$dbName = 'mszfrelbst_asodeck';
+$dbUsername = '';
+$dbPassword = '';
+$dbName = '';
 
 $conn = @new mysqli($serverName, $dbUsername, $dbPassword, $dbName);
 $conn->set_charset("utf8");
